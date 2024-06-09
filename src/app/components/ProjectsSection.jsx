96e8caff83bd8334aa0cddd3783bacefc7577b74
rizal15D <muhammadrizal15122001@gmail.com>
2024-06-09 22:16:36 +0700

    project section

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -1,34 +1,66 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
 
 const projectsData = [
   {
     id: 1,
     title: "E-Vote",
     description: "Project 1 description",
-    image: "/images/screenshot (305).png",
+    image: "/images/e-vote.png",
     tag: ["All", "Web"],
+    previewUrl: "/",
   },
   {
     id: 2,
     title: "Simple Wallet",
     description: "Project 2 description",
-    image: "/images/screenshot (307).png",
+    image: "/images/simple_wallet.png",
     tag: ["All", "Web", "Android"],
+    previewUrl: "https://simplewallet.agileteknik.com/",
   },
 ];
 const ProjectsSection = () => {
+  const [tag, setTag] = useState("All");
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+  const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag)
+  );
   return (
     <>
-      <h2>Projects</h2>
+      <h2 className="text-4xl font-bold text-white mb-4 text-center">
+        My Projects
+      </h2>
+      <div className="text-white flex flex-row justify-center item-center gap-2 py-6">
+        <ProjectTag
+          onClick={handleTagChange}
+          name="All"
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Web"
+          isSelected={tag === "Web"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Android"
+          isSelected={tag === "Android"}
+        />
+      </div>
       <div>
-        {projectsData.map((project) => (
+        {filteredProjects.map((project) => (
           <ProjectCard
             key={project.id}
             title={project.title}
             description={project.description}
             imgUrl={project.image}
             tag={project.tag}
+            previewUrl={project.previewUrl}
           />
         ))}
       </div>
